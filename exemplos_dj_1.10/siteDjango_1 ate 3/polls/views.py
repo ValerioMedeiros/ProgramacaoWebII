0f0d21da9be7b0f9907d355d89d55f2576c1c837
ValerioMedeiros <valerio.jr@gmail.com>
2016-10-22 23:30:45 -0300

    Tutorial parte 3 concluído

---
+++ exemplos_dj_1.10/siteDjango_1 ate 3/polls/views.py
@@ -0,0 +1,38 @@
+#from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template import loader
+from .models import Question
+
+"""
+def index(request):
+    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    #output = ', <br>'.join([q.question_text for q in latest_question_list])
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context,request))
+"""
+
+def index(request): #forma mais enxuta
+    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request,'polls/index.html',context)
+
+# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request,question_id):
+    response = "You'e looking at results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
