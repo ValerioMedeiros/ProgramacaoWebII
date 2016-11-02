15dd83a398da94161d49f19cd3b2555f50588f88
ValerioMedeiros <valerio.jr@gmail.com>
2016-11-02 13:58:55 -0300

    Tutorial 3 antes de começar (Removendo URLs codificados nos templates).

--- exemplos_dj_1.10/siteDjango_1 ate 3/polls/urls.py
+++ exemplos_dj_1.10/siteDjango_1 ate 3/polls/urls.py
@@ -5,10 +5,12 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    # /polls/
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # /polls/5
-    url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$',views.results, name='results'),
-    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote, name='vote'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
