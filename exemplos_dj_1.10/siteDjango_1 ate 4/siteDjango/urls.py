6dca0cdd7a7cf7a5ea4f412e425631336a0e6362
ValerioMedeiros <valerio.jr@gmail.com>
2016-11-02 17:14:12 -0300

    Parte 4 concluída do tutorial Django

--- exemplos_dj_1.10/siteDjango_1 ate 4/siteDjango/urls.py
+++ exemplos_dj_1.10/siteDjango_1 ate 4/siteDjango/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 
 urlpatterns = [
 
-    #url(r'^$', include('polls.urls')),#Redireciona a página principal para polls url
+    
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
