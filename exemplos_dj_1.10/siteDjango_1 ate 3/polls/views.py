d79d8761720a2324c26fa7b911b46adad5952d23
ValerioMedeiros <valerio.jr@gmail.com>
2016-11-02 13:58:55 -0300

    Tutorial 3 antes de começar (Removendo URLs codificados nos templates).

--- exemplos_dj_1.10/siteDjango_1 ate 3/polls/views.py
+++ exemplos_dj_1.10/siteDjango_1 ate 3/polls/views.py
@@ -16,15 +16,24 @@ def index(request):
     return HttpResponse(template.render(context,request))
 """
 
-def index(request): #forma mais enxuta
-    template = loader.get_template('polls/index.html')
+
+def index(request):#forma mais enxuta
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return render(request,'polls/index.html',context)
+    return HttpResponse(template.render(context, request))
 
 # Create your views here.
+""" #Versao com try e except
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Questao nao existe!")
+    return render(request,'polls/detail.html',{ 'question': question})
+"""
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
