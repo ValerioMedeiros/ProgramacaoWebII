bf7e80a1fa8eedc028f6186ffb1102cfaf1986f4
ValerioMedeiros <valerio.jr@gmail.com>
2016-12-22 14:01:13 -0300

    Adicionado testes

--- exemplos_dj_1.9/projetovendas/appvendas/tests.py
+++ exemplos_dj_1.9/projetovendas/appvendas/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+from django.utils import timezone
+from django.test import TestCase
+import time
+
+
+from selenium import webdriver
+
+
+
+
+class QuestionMethodTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() should return False for questions whose
+        pub_date is in the future.
+        """
+        import selenium.webdriver.chrome.service as service
+
+        i = '/Users/valerio/Downloads/imagem.png'
+        c = '/Users/valerio/Downloads/chromedriver'
+        g = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+        service = service.Service(c)
+        service.start()
+        capabilities = {'chrome.binary': g}
+        driver = webdriver.Remote(service.service_url, capabilities)
+        driver.get('http://localhost:8000/appvendas/');
+        time.sleep(3)  # Let the user actually see something!
+        driver.get('http://localhost:8000/appvendas/produtos');
+        time.sleep(3)  # Let the user actually see something!
+        driver.get('http://localhost:8000/appvendas/clientes');
+        time.sleep(3)  # Let the user actually see something!
+        """
+
+        driver.get('http://www.google.com/xhtml');
+        time.sleep(3)  # Let the user actually see something!
+
+        search_box = driver.find_element_by_name('q')
+        search_box.send_keys('Valerio Gutemberg ')
+        driver.get_screenshot_as_file(i)
+        search_box.submit()
+        time.sleep(10)  # Let the user actually see something!
+        """
+        driver.quit()
--- exemplos_dj_1.9/projetovendas/projetovendas/settings.py
+++ exemplos_dj_1.9/projetovendas/projetovendas/settings.py
@@ -39,7 +39,21 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'appvendas',
     'widget_tweaks', #Instalar o Widget tweaks https://pypi.python.org/pypi/django-widget-tweaks
+    'django_nose'  # Util para realizar a cobertura de código com o Nose
 ]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'  # Util para realizar a cobertura de código com o Nose
+NOSE_ARGS = [                                    # Util para realizar a cobertura de código com o Nose
+       '--with-coverage',  # activate coverage report
+        '--with-doctest',  # activate doctest: find and run docstests
+        '--verbosity=2',   # verbose output
+        '--with-xunit',    # enable XUnit plugin
+        '--xunit-file=xunittest.xml',  # the XUnit report file
+        '--cover-xml',     # produle XML coverage info
+        '--cover-xml-file=coverage.xml',  # the coverage info file
+        # You may also specify the packages to be covered here
+        # '--cover-package=blog,examples'
+    ]
 
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
